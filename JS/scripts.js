7b02df404b4ad67f768573c49f8c6e00ac7aee48
K1LL3R_DEV <Rodrigo>
2024-12-26 18:12:07 -0300

    Update_version(1.4)

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,6 @@
-function validacaoPedido(){
+function validacaoPedido() {
+
+    //Pedidos daa tabela
     const n1 = "design de sobrancelha"
     const n2 = "design com henna"
     const n3 = "depilação de buço"
@@ -8,43 +10,46 @@ function validacaoPedido(){
     const n7 = "depilação virilha"
     const n8 = "depilação virilha completa"
     const n9 = "depilação íntima completa"
-        let nServico = document.getElementById("servico").value
-        if (nServico == 1){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n1;
-        }
-        else if(nServico == 2){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n2;
-        }
-        else if(nServico == 3){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n3;
-    
-        }
-        else if(nServico == 4){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n4;
-            
-        }
-        else if(nServico == 5){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n5;
-            
-        }
-        else if(nServico == 6){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n6;
-            
-        }
-        else if(nServico == 7){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n7;
-            
-        }
-        else if(nServico == 8){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n8;
-            
-        }
-        else if(nServico == 9){
-            window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+"+n9;        
-        }
-        else{
-            alert ("Serviço invalido, verifique o serviço desejado na tabela e tente novamente")
-        }
+
+    //Filtro dos pedidos
+
+    let nServico = document.getElementById("servico").value
+    if (nServico == 1) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n1;
+    }
+    else if (nServico == 2) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n2;
+    }
+    else if (nServico == 3) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n3;
+
+    }
+    else if (nServico == 4) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n4;
+
+    }
+    else if (nServico == 5) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n5;
+
+    }
+    else if (nServico == 6) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n6;
+
+    }
+    else if (nServico == 7) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n7;
+
+    }
+    else if (nServico == 8) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n8;
+
+    }
+    else if (nServico == 9) {
+        window.location.href = "https://wa.me/5581986487802/?text=Olá,+gostaria+de+agendar+" + n9;
+    }
+    else {
+        alert("Serviço invalido, verifique o serviço desejado na tabela e tente novamente")
+    }
 } // Validação de pedido do usuario 
 
 function enviarDuvida() {
